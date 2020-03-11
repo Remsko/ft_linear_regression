7eb53bbfb0c5af43c25592edcbde3f46e98bbbea
Remsko <pinoit.remi@gmail.com>
2020-03-11 12:41:04 +0100

    bonuses

--- train.py
+++ train.py
@@ -1,19 +1,24 @@
+import sys
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+g_cost_history = []
+g_theta_history = []
 
 def average(x):
     return sum(x) / len(x)
 
+def extremum_sub(x):
+    return max(x) - min(x)
 
 def mean_normalize(x):
-    return (x - average(x)) / (max(x) - min(x))
+    return (x - average(x)) / extremum_sub(x)
 
 
 def mean_reverse(theta0, theta1, x):
-    theta0 = theta0 - theta1 * average(x) / (max(x) - min(x))
-    theta1 = theta1 /(max(x) - min(x))
+    theta0 = theta0 - theta1 * average(x) / extremum_sub(x)
+    theta1 = theta1 / extremum_sub(x)
     return theta0, theta1
 
 
@@ -28,38 +33,55 @@ def cost(theta0, theta1, x, y):
     return cost
 
 
-def gradient_descent(theta0, theta1, x, y, alpha=0.01, cycle=10000):
+def gradient_descent(theta0, theta1, x, y, alpha=0.1, cycle=1000, step=20):
     m = len(x)
-    cost_history = []
-    for _ in range(cycle):
+    for i in range(cycle):
         prediction = predict(theta0, theta1, x)
         tmp_theta0 = alpha * (1 / m) * sum(prediction - y)
         tmp_theta1 = alpha * (1 / m) * sum((prediction - y) * x)
         theta0 -= tmp_theta0
         theta1 -= tmp_theta1
-        cost_history.append(cost(theta0, theta1, x, y))
-    return theta0, theta1, cost_history
+        g_cost_history.append(cost(theta0, theta1, x, y))
+        if i % (cycle / step) == 0:
+            g_theta_history.append([theta0, theta1])
+    return theta0, theta1
 
 
-def cost_history_display(figure, cost_history):
-    cycles = list(range(len(cost_history)))
-
-    axes = figure.add_subplot(2, 1, 1)
+def cost_history_display(figure):
+    cycles = list(range(len(g_cost_history)))
+    # Split
+    if "-v" in sys.argv:
+        axes = figure.add_subplot(2, 1, 1)
+    else:
+        axes = figure.add_subplot(1, 1, 1)
+    # Meta
     axes.set_xlabel('iteration')
     axes.set_ylabel('cost')
-    axes.set_title('Cost history')
-    
-    axes.plot(cycles, cost_history, color='green')
+    axes.set_title('Cost Evolution')
+    # Cost Evolution
+    axes.plot(cycles, g_cost_history, color='green')
 
 
 def linear_regression_display(figure, columns, x, y, theta0, theta1):
-    axes = figure.add_subplot(2, 1, 2)
+    # Split
+    if "-c" in sys.argv:
+        axes = figure.add_subplot(2, 1, 2)
+    else:
+        axes = figure.add_subplot(1, 1, 1)
+    # Meta
     axes.set_xlabel(columns[0])
     axes.set_ylabel(columns[1])
     axes.set_title('Prediction of car price given their mileages')
-
-    axes.scatter(x, y, label='Scatter Plot', color='red')
-    axes.plot(x, predict(theta0, theta1, x), label='Regression Line')
+    # Data display
+    axes.scatter(x, y, label='Scatter Plot', color='blue')
+    # Regression Line
+    if "-l" in sys.argv:
+        axes.plot(x, predict(theta0, theta1, x), label='Regression Line', color='#840606')
+    # Regression evolution
+    if "-h" in sys.argv:
+        for theta in g_theta_history:
+            theta0, theta1 = mean_reverse(theta[0], theta[1], x)
+            axes.plot(x, predict(theta0, theta1, x), color='#84060688')
     axes.legend()
 
 
@@ -67,24 +89,44 @@ def create_displayer():
     figure = plt.figure('Linear Regression', figsize=(10, 10))
     plt.gcf().subplots_adjust(left=0.09, bottom=0.07,
                               right=0.96, top=0.96, wspace=0, hspace=0.25)
-
     return figure
 
 
-if __name__ == "__main__":
-    data = pd.read_csv('data.csv')
+def train(data):
+    # Entries
     columns = data.columns.values[:2]
-
     x = np.array(data[columns[0]].values)
     y = np.array(data[columns[1]].values)
-
+    # Treatement
     norm_x = mean_normalize(x)
-
-    theta0, theta1, cost_history = gradient_descent(0, 0, norm_x, y)
+    theta0, theta1 = gradient_descent(0, 0, norm_x, y)
     theta0, theta1 = mean_reverse(theta0, theta1, x)
+    return x, y, theta0, theta1
 
+def display(data, x, y, theta0, theta1):
     figure = create_displayer()
-    cost_history_display(figure, cost_history)
-    linear_regression_display(figure, columns,x, y, theta0, theta1)
-
+    # Cost Evolution
+    if "-c" in sys.argv:
+        cost_history_display(figure)
+    # Graph
+    if "-v" or "-l" or "-h" in sys.argv:
+        columns = data.columns.values[:2]
+        linear_regression_display(figure, columns, x, y, theta0, theta1)
     plt.show()
+
+def main():
+    # Parsing
+    try:
+        data = pd.read_csv('data.csv')
+    except:
+        print("Failed to parse dataset !")
+        return -1
+    # Training
+    x, y, theta0, theta1 = train(data)
+    # Display
+    if "-c" or "-v" or "-l" or "-h" in sys.argv:
+        display(data, x, y, theta0, theta1)
+
+
+if __name__ == "__main__":
+    main()
